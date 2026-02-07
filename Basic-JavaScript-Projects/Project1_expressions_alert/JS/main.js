842927edd55e86729a12613c1df8119810ad6ab6
Isaac <Isaacgchaniel@icloud.com>
2026-02-07 18:14:05 +0000

    Writing Basic Javascript Assignment

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+//This code utlises the document.write() method
+let family = "Alexs",
+  Dad = "John",
+  Mum = "Silva",
+  Son = "Kenneth",
+  Daughter = "Penny";
+document.write(Son);
+
+//This code utilises window.alert() method
+window.alert("Hello, World");
+
+//Creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//Using Concatenation
+document.write(Sent1 + Sent2);
+
+//Writing an expression
+document.write(10 + 3);
