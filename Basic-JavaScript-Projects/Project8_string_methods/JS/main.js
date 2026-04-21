4483ad9fe52e83d19e997b6a4a5ebfc1fa69ddc7
Isaac <Isaacgchaniel@icloud.com>
2026-04-21 15:20:47 +0100

    JS project that includes some of the string method

---
+++ Basic-JavaScript-Projects/Project8_string_methods/JS/main.js
@@ -0,0 +1,35 @@
+//Using a concat() method
+function full_Sentence() {
+  var part_1 = "I have ";
+  var part_2 = "made this ";
+  var part_3 = "into a complete ";
+  var part_4 = "sentence. ";
+  var whole_Sentence = part_1.concat(part_2, part_3, part_4);
+  document.getElementById("Concatenate").innerHTML = whole_Sentence;
+}
+
+//Using a slice() method
+function slice_Method() {
+  var Sentence = "All work and no play makes Johnny a dull boy.";
+  var Section = Sentence.slice(27, 33);
+  document.getElementById("slice").innerHTML = Section;
+}
+
+//Using a toUpperCase() method
+function upper_Case() {
+  var text = "Hello World!";
+  var result = text.toUpperCase();
+  document.getElementById("upper").innerHTML = result;
+}
+
+//Using a Number to String() method
+function string_Method() {
+  var X = 182;
+  document.getElementById("Numbers_to_string").innerHTML = X.toString();
+}
+
+//Using a toPrecision() method
+function precision_method() {
+  var X = 12938.3012987376112;
+  document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
